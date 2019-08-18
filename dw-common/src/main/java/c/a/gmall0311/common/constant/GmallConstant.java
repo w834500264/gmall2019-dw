0190d8085a66c01c5ed147f290cda8b1a1763f4d
wang834500264 <834500264@qq.com>
2019-08-18 09:08:28 +0800

    Initial commit

---
+++ dw-common/src/main/java/c/a/gmall0311/common/constant/GmallConstant.java
@@ -0,0 +1,7 @@
+package c.a.gmall0311.common.constant;
+
+public class GmallConstant {
+    public static final String KAFKA_TOPIC_STARTUP="GMALL_STARTUP";
+    public static final String KAFKA_TOPIC_EVENT="GMALL_EVENT";
+
+}
---
+++ gmall0311-logger/src/main/java/c/a/gmall0311/logger/controller/LoggerController.java
@@ -0,0 +1,39 @@
+package c.a.gmall0311.logger.controller;
+
+
+
+import c.a.gmall0311.common.constant.GmallConstant;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RestController//==@Controller + @ResponseBody
+public class LoggerController {
+
+    @Autowired
+    KafkaTemplate<String,String>kafkaTemplate;
+
+    @PostMapping("log")
+    public String dolog(@RequestParam("logString") String logString){
+        //1 补充时间戳
+        JSONObject jsonObject = JSON.parseObject(logString);
+        jsonObject.put("ts",System.currentTimeMillis());
+        //2 写日志(用于离线采集)
+        String logJson = jsonObject.toJSONString();
+        log.info(logJson);
+        //3 发送kafka
+        if ("startup".equals(jsonObject.getString("type"))){
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_STARTUP,logJson);
+        }else{
+            kafkaTemplate.send(GmallConstant.KAFKA_TOPIC_EVENT,logJson);
+        }
+
+        return "success";
+
+    }
+}
